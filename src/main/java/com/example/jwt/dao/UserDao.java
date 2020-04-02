a5d735df4b4959aaeda4e3e2f694a445eadc52b6
Sofia Contreras <sofia.contreras@itexico.com>
2020-04-02 15:56:13 -0600

    User services impl

--- src/main/java/com/example/jwt/dao/UserDao.java
+++ src/main/java/com/example/jwt/dao/UserDao.java
@@ -4,4 +4,5 @@ import com.example.jwt.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDao extends CrudRepository<User, Integer> {
+    User findByUsername(String username);
 }
--- src/main/java/com/example/jwt/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/jwt/service/impl/UserServiceImpl.java
@@ -1,17 +1,29 @@
 package com.example.jwt.service.impl;
 
+import com.example.jwt.dao.UserDao;
 import com.example.jwt.dto.UserDto;
+import com.example.jwt.entity.Employee;
 import com.example.jwt.entity.User;
 import com.example.jwt.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
     @Override
     public User addUser(UserDto userDto) {
-        return null;
+        User user = new User();
+        BeanUtils.copyProperties(userDto, user);
+        return userDao.save(user);
     }
 
     @Override
     public User getUser(String username) {
-        return null;
+        return userDao.findByUsername(username);
     }
 }
